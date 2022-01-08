3193c97f799c24c06b157037334fc68683909fab
mdiegoli <morticcino@diegoli.org>
2022-01-08 11:29:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-tcbpfs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-tcbpfs)
---
+++ src/app/app.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  name = 'Angular';
+
+  ngOnInit() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    const renderer_domElement = renderer.domElement;
+    document.body.appendChild(renderer_domElement);
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    const sphereGeometry = new THREE.SphereGeometry(100, 100, 100);
+
+    const wireframe = new THREE.WireframeGeometry(sphereGeometry);
+
+    const line = new THREE.LineSegments(wireframe);
+    line.material.depthTest = false;
+    line.material.opacity = 0.25;
+    line.material.transparent = true;
+
+    scene.add(line);
+    /*
+    const animate = function () {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+    camera.position.z = 5;
+    renderer.render(scene, camera);
+    animate();
+    */
+
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+
+    function onMouseMove(event) {
+      // calculate mouse position in normalized device coordinates
+      // (-1 to +1) for both components
+      mouse.x =
+        ((event.clientX - renderer_domElement.offsetLeft) /
+          renderer_domElement.clientWidth) *
+          2 -
+        1;
+      mouse.y =
+        -(
+          (event.clientY - renderer_domElement.offsetTop) /
+          renderer_domElement.clientHeight
+        ) *
+          2 +
+        1;
+    }
+
+    function render() {
+      window.requestAnimationFrame(render);
+      // update the picking ray with the camera and mouse position
+      raycaster.setFromCamera(mouse, camera);
+
+      // calculate objects intersecting the picking ray
+      const intersects = raycaster.intersectObjects(scene.children);
+
+      for (let i = 0; i < intersects.length; i++) {
+        //intersects[ i ].object.material.color.set( 0xff0000 );
+        cube.position.copy(intersects[i].point);
+      }
+
+      renderer.render(scene, camera);
+    }
+
+    window.addEventListener('mousemove', onMouseMove, false);
+    render();
+  }
+}
